d95d22c7ff462f8a463925f4c46f6c198353304a
leozhang2018 <leozhang2018@gmail.com>
2016-10-29 16:23:57 +0800

    Add a href to case description

--- js/case/customData.js
+++ js/case/customData.js
@@ -4,7 +4,7 @@ var customerData = [{
     href: '/case/camera360',
     poster: 'https://oerugfbxb.qnssl.com/camera360.jpg',
     project_bg: '与七牛的结缘是在 2012 年，当时两家公司都处于创业初期。作为创业公司，Camera360 必须将研发精力集中在产品业务上，所以对所选云服务的可用性、易用性、扩展性等有非常苛刻的要求。尤其是 Camera360 服务的用户是过亿级别，哪怕是一秒钟的闪断都是不能容忍的……',
-    product_using: ['对象存储 http://www.qiniu.com/products/kodo', '融合 CDN http://www.qiniu.com/products/fusion', '数据处理 http://www.qiniu.com/products/dora']
+    product_using: [{ name: '对象存储', url: 'http://www.qiniu.com/products/kodo' }, { name: '融合 CDN', url: 'http://www.qiniu.com/products/fusion' }, { name: '数据处理', url: 'http://www.qiniu.com/products/dora' }]
 
 }, {
     id: '1',
@@ -12,7 +12,7 @@ var customerData = [{
     href: '/case/meipai',
     poster: 'https://oerugfbxb.qnssl.com/meipai.jpg',
     project_bg: '美拍于 2014 年 5 月发布，上线仅 1 天便登入 App Store 免费总榜第一，当月下载量排名第一。在发布 9 个月的时候，用户突破 1 亿。截至 2015 年 10 月 31 日，美拍视频日播放量在 2.7 亿以上，日视频播放时长达到 183 万小时。在面临用户量爆发式增长的情况下……',
-    product_using: ['对象存储 http://www.qiniu.com/products/kodo', '直播云服务 http://www.qiniu.com/products/pili' ,'融合 CDN http://www.qiniu.com/products/fusion', '数据处理 http://www.qiniu.com/products/dora']
+    product_using: [{ name: '对象存储', url: 'http://www.qiniu.com/products/kodo' }, { name: '直播云服务', url: 'http://www.qiniu.com/products/pili' }, { name: '融合 CDN', url: 'http://www.qiniu.com/products/fusion' }, { name: '数据处理', url: 'http://www.qiniu.com/products/dora' }]
 
 }, {
     id: '2',
@@ -20,7 +20,7 @@ var customerData = [{
     href: '/case/zte',
     poster: 'https://oerugfbxb.qnssl.com/zte.jpg',
     project_bg: '恰如当年为抢夺移动互联网入口而引发的智能手机大战，随着物联网及互联网+的迅猛发展，不少企业敏锐地觉察到，家庭将成为智能手机后的又一全新物联网入口。在如此关键的时间点，谷歌 32 亿美金高价收购智能家居企业 Nest Lab 就成为导火索，引爆了全球智能家居市场。苹果……',
-    product_using: ['对象存储 http://www.qiniu.com/products/kodo', '直播云服务 http://www.qiniu.com/products/pili']
+    product_using: [{ name: '对象存储', url: 'http://www.qiniu.com/products/kodo' }, { name: '直播云服务', url: 'http://www.qiniu.com/products/pili' }]
 
 }, {
     id: '3',
@@ -28,7 +28,7 @@ var customerData = [{
     href: '/case/pandatv',
     poster: 'https://oerugfbxb.qnssl.com/panda.jpg',
     project_bg: '随着移动直播的蓬勃发展，人们迎来了内容消费升级的时代，从文字、图片、音频、视频一步步升级演化，人们对视觉无止境的追求呼唤了越来越多的手机直播 APP 如雨后春笋般涌现。现今移动直播技术上的挑战要远远难于传统设备和电脑之间的直播，这就决定了移动直播在实现……',
-    product_using: ['对象存储 http://www.qiniu.com/products/kodo', '融合 CDN http://www.qiniu.com/products/fusion', '直播云服务 http://www.qiniu.com/products/pili']
+    product_using: [{ name: '对象存储', url: 'http://www.qiniu.com/products/kodo' }, { name: '融合 CDN', url: 'http://www.qiniu.com/products/fusion' }, { name: '直播云服务', url: 'http://www.qiniu.com/products/pili' }]
 
 }, {
     id: '4',
@@ -36,5 +36,5 @@ var customerData = [{
     href: '/case/xiaoying',
     poster: 'https://oerugfbxb.qnssl.com/xiaoying.jpg',
     project_bg: '对于用户而言，视频的体验，一方面是因为他们的生活方式更加适合用视频记录，另一方面是因为他们有记录生活以及让更多人看到的需求。从用户分布情况来看，与美拍、秒拍及快手相比，小影的用户大部分在海外，且海外用户规模占到了 75% 的比例。小影在整个移动视频产……',
-    product_using: ['对象存储 http://www.qiniu.com/products/kodo', '融合 CDN http://www.qiniu.com/products/fusion', '数据处理 http://www.qiniu.com/products/dora']
+    product_using: [{ name: '对象存储', url: 'http://www.qiniu.com/products/kodo' }, { name: '融合 CDN', url: 'http://www.qiniu.com/products/fusion' }, { name: '数据处理', url: 'http://www.qiniu.com/products/dora' }]
 }];
